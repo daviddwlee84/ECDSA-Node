e6ce57613e238c2d0bee13e106f663da4b414fa8
Da-Wei Lee <daviddwlee84@gmail.com>
2024-09-14 21:05:31 +0800

    Able to verify transaction and recover public key from given signature

--- server/scripts/generate_new.js
+++ server/scripts/generate_new.js
@@ -4,6 +4,7 @@ const {
   getAddressFromPublicKey,
   getHexAddressFromPublicKey,
   toHex,
+  hashTransaction,
   verifyTransaction,
 } = require('../utils/key');
 const { secp256k1 } = require('ethereum-cryptography/secp256k1');
@@ -12,7 +13,7 @@ const { utf8ToBytes } = require('ethereum-cryptography/utils');
 
 const privateKey = getRandomPrivateKey();
 
-console.log("private key:", toHex(privateKey));
+console.log("private key:", toHex(privateKey), typeof privateKey);
 
 const publicKey = getPublicKeyFromPrivateKey(privateKey);
 
@@ -21,15 +22,12 @@ console.log("full public key:", toHex(publicKey));
 console.log("public key (address) (old):", toHex(getAddressFromPublicKey(publicKey)));
 console.log("public key (address):", getHexAddressFromPublicKey(publicKey));
 
-function hashTransaction(sender, amount, recipient) {
-  return keccak256(utf8ToBytes(`${sender} transfer ${amount} to ${recipient}`))
-}
 
 function signTransactionHash(txHash, privateKey) {
   return secp256k1.sign(txHash, privateKey);
 }
 
-const txHash = hashTransaction('0x1', 20, '0x2');
+const txHash = hashTransaction(20, '0x2');
 console.log("transaction hash:", toHex(txHash));
 
 
@@ -40,3 +38,5 @@ signature = signTransactionHash(txHash, privateKey);
 const recoveredPublicKey = signature.recoverPublicKey(txHash).toHex();
 console.log("recovered full public key:", recoveredPublicKey);
 console.log("the public key are matched:", toHex(publicKey) === recoveredPublicKey);
+
+console.log(verifyTransaction(signature, 20, '0x2'));
--- server/utils/key.js
+++ server/utils/key.js
@@ -1,5 +1,5 @@
 const { secp256k1 } = require("ethereum-cryptography/secp256k1");
-const { toHex } = require("ethereum-cryptography/utils");
+const { toHex, hexToBytes, utf8ToBytes } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 
 const LAST_K_BYTES_AS_ADDR = 1;
@@ -18,8 +18,26 @@ function getPublicKeyFromPrivateKey(privateKey) {
 function getAddressFromPublicKey(publicKey) {
   return keccak256(publicKey.slice(1)).slice(-LAST_K_BYTES_AS_ADDR);
 }
+
 function getHexAddressFromPublicKey(publicKey) {
   return `0x${toHex(getAddressFromPublicKey(publicKey)).slice(-LAST_K_DIGITS_AS_ADDR)}`
+}
+
+function hashTransaction(amount, recipient) {
+  return keccak256(utf8ToBytes(`Transfer ${amount} to ${recipient}`));
+  // return keccak256(utf8ToBytes(JSON.stringify({ amount: amount, recipient: recipient })));
+}
+
+function verifyTransaction(signature, amount, recipient) {
+  sign = new secp256k1.Signature(signature.r, signature.s, signature.recovery);
+  txHash = hashTransaction(amount, recipient);
+  const recoveredPublicKey = sign.recoverPublicKey(txHash).toHex();
+  const sender = getHexAddressFromPublicKey(hexToBytes(recoveredPublicKey));
+
+  const transactionHash = keccak256(utf8ToBytes(`Transfer ${amount} to ${recipient}`));
+  // const transactionHash = keccak256(utf8ToBytes(JSON.stringify({ amount: amount, recipient: recipient })))
+  const isSigned = secp256k1.verify(sign, transactionHash, recoveredPublicKey);
+  return [isSigned, sender];
 }
 
 module.exports = {
@@ -28,4 +46,6 @@ module.exports = {
   getAddressFromPublicKey,
   getHexAddressFromPublicKey,
   toHex,
+  hashTransaction,
+  verifyTransaction,
 }
